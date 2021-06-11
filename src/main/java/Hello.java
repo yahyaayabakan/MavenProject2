ef103f929046232d99852d51bcfe61ca658e3725
yahyaayabakan <ayabakanyahya@gmail.com>
2021-06-11 16:51:40 +0100

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local Repo is already published");
+        System.out.println("Doing more work");
     }
 }
